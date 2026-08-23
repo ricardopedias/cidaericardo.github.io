bdb10231287b4620a7f1c9646c053b26b92aefd6
Ricardo Dias <contato@ricardopedias.com.br>
2026-08-23 09:58:09 -0300

    Tela cheia

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ function abrirEmTelaCheia()
 {
   let elem = document.body;
 
-  console.log('fullscreen');
-  
+  alert('fullscreen');
+
   // ## The below if statement seems to work better ## if ((document.fullScreenElement && document.fullScreenElement !== null) || (document.msfullscreenElement && document.msfullscreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
   if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) || (document.mozFullScreen !== undefined && !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) {
       if (elem.requestFullScreen) {
